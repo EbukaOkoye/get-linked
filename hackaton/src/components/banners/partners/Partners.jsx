f6feaad810a34cf3906637e4a4d2550e58438f32
EbukaOkoye <chukwuebukaokoye408@gmail.com>
2023-09-21 19:34:51 +0100

    pushing landing page

---
+++ hackaton/src/components/banners/partners/Partners.jsx
@@ -0,0 +1,22 @@
+import { partner, purple } from "../../../utilities/utils";
+import "../banner.css";
+
+const Partners = () => {
+  return (
+    <>
+      <div className="bg-mainBlue p-4 left-banner-left banner-left-blur">
+        <h2 className="text-center text-white lg:text-3xl text-xl font-bold font-custom mt-24">
+          Partners and Sponsors
+        </h2>
+        <p className="text-center text-white text-sm font-mont my-2">
+          Getlinked Hackathon 1.0 is honored to have the following major
+          companies as its partners and sponsors
+        </p>
+        <img className="w-4 h-4 mt-10 ml-16" src={purple} alt="" />
+        <img className="mt-8 mb-16" src={partner} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Partners;
---
+++ hackaton/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['Clash Display',],
+        mont: 'Montserrat'
+      },
+      colors: {
+        mainBlue: '#150E28',
+        brightPurple: '#D434FE',
+        lightBlue: '#903AFF',
+        redPink: '#FF26B9',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
